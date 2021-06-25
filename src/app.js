463a459f1afded74d38387d45bbdc758f91cd345
Jacinto Molina <43961698+chinto40@users.noreply.github.com>
2021-06-25 11:44:51 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -4,3 +4,8 @@ const express = require('express')
 
 
 const app = express()
+app.set(express.static(path.join(__dirname, 'public/')))
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('view engine', 'ejs')
+
