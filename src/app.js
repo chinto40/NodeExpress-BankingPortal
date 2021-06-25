43e19529376173b1c80df726e4038ed6d13b7218
Jacinto Molina <43961698+chinto40@users.noreply.github.com>
2021-06-25 11:54:40 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,17 @@ const express = require('express')
 
 
 const app = express()
-app.set(express.static(path.join(__dirname, 'public/')))
-app.set('views', path.join(__dirname, 'views'))
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/',(req,res)=>{
+    res.render('index', {title: 'Index'})
+})
+
+app.listen(3000, ()=>{
+    console.log('PS Project Running on Port 3000!')
+});
 
